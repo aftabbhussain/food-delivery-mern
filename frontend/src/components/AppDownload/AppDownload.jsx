07ca3a1b7c38c370ff4a013900cc75907881e4f1
aftabbhussain <88221488+aftabbhussain@users.noreply.github.com>
2026-02-04 00:12:08 +0530

    Finalize the Frontend -Completed Building the Frontend -Made the website responsive for all screen types

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets'
-
+import './AppDownload.css'
 const AppDownload = () => {
   return (
     <div className='app-download' id='app-download'>
